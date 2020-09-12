f65838a38260bded81e316036b37e9355a3f4ebc
Aleks Popovic <aleksandar88popovic@gmail.com>
2020-09-12 12:01:24 +0200

    Dark Mode project upload

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./styles/App.css";
+import DarkMode from "./components/DarkMode";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/">Projects</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <DarkMode />
+      </nav>
+      <h1>Hello World</h1>
+      <div id="image"></div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget
+        scelerisque neque, quis scelerisque erat. Quisque venenatis molestie
+        sapien, dapibus viverra nulla hendrerit eget. Pellentesque egestas
+        ultrices accumsan. Ut ac magna vel ex maximus ultricies. Nulla facilisi.
+        Suspendisse gravida sem eu odio mattis ullamcorper. Curabitur feugiat
+        ipsum vel vulputate ultricies.
+      </p>
+      <p>
+        Praesent pulvinar faucibus risus in iaculis. Sed erat felis, pretium sit
+        amet ultricies non, porta et lacus. Curabitur a urna mi. Sed eleifend
+        sed erat eget viverra. Quisque sit amet purus viverra massa posuere
+        congue. Suspendisse efficitur venenatis enim, id hendrerit enim ultrices
+        sed. Nam sed dapibus nisi.
+      </p>
+      <p>
+        Proin venenatis vulputate dolor at condimentum. Sed posuere lacus eu
+        nisi elementum, in bibendum ipsum elementum. Etiam scelerisque vel
+        lectus quis ornare. Vestibulum id molestie nibh, sit amet tincidunt
+        sapien. Pellentesque eu arcu accumsan, aliquam eros non, bibendum erat.
+        Vestibulum ultricies at felis in porttitor. Suspendisse potenti.
+        Praesent nec dignissim nisl. Nunc felis turpis, scelerisque in rhoncus
+        ac, mattis ut orci. Proin elementum massa quam, nec facilisis odio
+        rhoncus a. Maecenas efficitur semper mi, sed porta massa malesuada sit
+        amet. Morbi blandit consequat elementum.
+      </p>
+      <p>
+        Labore ullamco tempor laborum officia enim anim exercitation consectetur
+        pariatur minim non officia proident. Ipsum est reprehenderit qui aute id
+        enim amet ullamco aliqua magna excepteur commodo ut. Ut culpa labore
+        cillum sit non ullamco qui.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget
+        scelerisque neque, quis scelerisque erat. Quisque venenatis molestie
+        sapien, dapibus viverra nulla hendrerit eget. Pellentesque egestas
+        ultrices accumsan. Ut ac magna vel ex maximus ultricies. Nulla facilisi.
+        Suspendisse gravida sem eu odio mattis ullamcorper. Curabitur feugiat
+        ipsum vel vulputate ultricies.
+      </p>
+      <p>
+        Praesent pulvinar faucibus risus in iaculis. Sed erat felis, pretium sit
+        amet ultricies non, porta et lacus. Curabitur a urna mi. Sed eleifend
+        sed erat eget viverra. Quisque sit amet purus viverra massa posuere
+        congue. Suspendisse efficitur venenatis enim, id hendrerit enim ultrices
+        sed. Nam sed dapibus nisi.
+      </p>
+      <p>
+        Proin venenatis vulputate dolor at condimentum. Sed posuere lacus eu
+        nisi elementum, in bibendum ipsum elementum. Etiam scelerisque vel
+        lectus quis ornare. Vestibulum id molestie nibh, sit amet tincidunt
+        sapien. Pellentesque eu arcu accumsan, aliquam eros non, bibendum erat.
+        Vestibulum ultricies at felis in porttitor. Suspendisse potenti.
+        Praesent nec dignissim nisl. Nunc felis turpis, scelerisque in rhoncus
+        ac, mattis ut orci. Proin elementum massa quam, nec facilisis odio
+        rhoncus a. Maecenas efficitur semper mi, sed porta massa malesuada sit
+        amet. Morbi blandit consequat elementum.
+      </p>
+      <p>
+        Labore ullamco tempor laborum officia enim anim exercitation consectetur
+        pariatur minim non officia proident. Ipsum est reprehenderit qui aute id
+        enim amet ullamco aliqua magna excepteur commodo ut. Ut culpa labore
+        cillum sit non ullamco qui.
+      </p>
+    </div>
+  );
+}
+
+export default App;
